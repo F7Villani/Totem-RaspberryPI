e92fdc9f3ee22d18de3ac35295a2820622dd0bd2
Renan Reschke <renanreschke@hotmail.com>
2022-09-20 17:09:22 -0300

    back 1.0

--- back-end/index.js
+++ back-end/index.js
@@ -1,19 +1,13 @@
 import express from 'express'
 import cors from 'cors'
-import { UCAddHelpOffer } from './usecases/add-help-offer.js'
-import { UCAddInterest } from './usecases/add-interest.js'
-import { UCConsultInterest } from './usecases/consult-interests.js'
+import { UCAddOrder } from './usecases/add-order.js'
+import { UCGetProductsForResume } from './usecases/get-products-for-resume.js'
+import { UCGetProducts } from './usecases/get-products.js'
 import { UCConsultHelpOffer } from './usecases/consult-help-offers.js'
-import { VolatileRepo } from './repository/volatile.js'
 import { MongoDB } from './repository/mongodb.js'
-import { HelpOffer } from './models/help-offer.js'
-import { Interest } from './models/interest.js'
-import { DateTime } from 'Luxon'
-import { HelpTypeEnum } from './enums/help-type.js'
-import { CPostHelpOffer } from './controllers/get-products.js'
-import { CPostInterest } from './controllers/post-interest.js'
-import { CGetInterests } from './controllers/get-products-for-resume.js'
-import { CGetHelpOffers } from './controllers/get-help-offer.js'
+import { CGetProducts } from './controllers/get-products.js'
+import { CGetProductsForResume } from './controllers/get-products-for-resume.js'
+import { CPostOrder } from './controllers/post-order.js'
 
 
 const app = express()
@@ -23,32 +17,25 @@ app.use(cors())
 
 const repo = new MongoDB() //Define o tipo de repositório a ser usado
 
-const ucAddInterest = new UCAddInterest(repo)
-const cPostInterest = new CPostInterest(ucAddInterest)
+const uCAddOrder = new UCAddOrder(repo)
+const cPostOrder = new CPostOrder(uCAddOrder)
 
-const ucConsultInterest = new UCConsultInterest(repo)
-const cGetInterests = new CGetInterests(ucConsultInterest)
+const uCGetProductsForResume = new UCGetProductsForResume(repo)
+const cGetProductsForResume = new CGetProductsForResume(uCGetProductsForResume)
 
-const ucAddHelpOffer = new UCAddHelpOffer(repo)
-const cPostHelpOffer = new CPostHelpOffer(ucAddHelpOffer)
+const uCGetProducts = new UCGetProducts(repo)
+const cGetProducts = new CPostHelpOffer(uCGetProducts)
 
-const ucConsultHelpOffers = new UCConsultHelpOffer(repo)
-const cGetHelpOffer = new CGetHelpOffers(ucConsultHelpOffers)
-
-app.post('/help-offer', async (req, res) => {
-    res.sendStatus(201).send(await cPostHelpOffer.postHelpOffer(req.body))
+app.post('/order', async (req, res) => {
+    res.sendStatus(201).send(await cPostOrder.postOrder(req.body))
 })
 
-app.post('/interest', async (req, res) => {
-    res.sendStatus(201).send(await cPostInterest.postInterest(req.body))
+app.get('/products', async (req, res) => {
+    res.send(await cGetProducts.getProducts(req.body))
 })
 
-app.get('/help-offers', async (req, res) => {
-    res.send(await cGetHelpOffer.getHelpOffers(req.body))
-})
-
-app.get('/interests', async (req, res) => {
-    res.send(await cGetInterests.getInterest(req.body))
+app.get('/products-resume', async (req, res) => {
+    res.send(await cGetProductsForResume.getProductsForResume(req.body))
 })
 
 app.listen(8080, () => {
