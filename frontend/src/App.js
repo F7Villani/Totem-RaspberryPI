7b082060ceaca18869f0b6f82f33b53769cbe251
Felipe Villani <felipevillani.2000@hotmail.com>
2022-09-23 20:46:31 -0300

    feat: adicionando os novos componentes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
-import { CategoryButton } from './components/CategoryButton';
-import { Counter } from './components/Counter/Counter';
+import { CategoryButton } from './components/CategoryButton/CategoryButton';
 import { CleanOrderButton} from './components/CleanOrderButton/CleanOrderButton';
-import PayButton from './components/PayButton/PayButton';
+import { PayButton } from './components/PayButton/PayButton';
+import { CardItemSelect } from './components/CardItemSelect/CardItemSelect';
 
 
 const App = () => {
     return(
-        <div>            
-            <CategoryButton imageName='Vetor_Combo.png' category='Combos'/>
-            <Counter/>
+        <div>     
+            <h1>Titulo</h1>       
+            <CategoryButton imageName='Vetor_Combo.png' category='Combos' onClick={() =>{
+                console.log("Fui passado pelo props!!!")
+            }}/>
+  
+            <CardItemSelect itemName='Combo 1' price='30,9' imageName='Vetor_Sorvete.png' />
+            <CardItemSelect itemName='Combo 1' price='30,9' imageName='Vetor_Sorvete.png' />
+            <CardItemSelect itemName='Combo 1' price='30,9' imageName='Vetor_Sorvete.png' />
             <CleanOrderButton amountItems='5'/>
             <PayButton/>
         </div>
