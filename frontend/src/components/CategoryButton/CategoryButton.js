524d8c30a735e24ad1d67cfc4865c0490c8bdf1e
Felipe Villani <felipevillani.2000@hotmail.com>
2022-09-23 20:43:41 -0300

    feat: iniciando css do componente e colocando ele na pasta

--- frontend/src/components/CategoryButton/CategoryButton.js
+++ frontend/src/components/CategoryButton/CategoryButton.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import "primereact/resources/themes/lara-light-indigo/theme.css";  
 import "primereact/resources/primereact.min.css";                  
 import "primeicons/primeicons.css";                                
+import './CategoryButton.css';
 
 import { Button } from 'primereact/button';
 
@@ -14,13 +15,14 @@ export class CategoryButton extends Component {
 
     render() {
         return (
-        <React.Fragment> 
+        <React.Fragment className='category-button'> 
             <Button 
                 className='p-button-raised p-button-secondary'
                 onClick={ () => {
+                    this.props.onClick();
                     console.log(`TODO: Vai pra pagina de escolher ${this.props.category}`)}}>
                 <div>
-                    <img src={require('../assets/images/' + this.props.imageName)}></img>
+                    <img src={require('../../assets/images/' + this.props.imageName)}></img>
                     <h1>{this.props.category}</h1>
                 </div>
             </Button>       
