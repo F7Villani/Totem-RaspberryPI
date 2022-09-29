d1be82961cdd8bab7ed0314cb24629fb32442090
Renan Reschke <renanreschke@hotmail.com>
2022-09-29 13:56:17 -0300

    feat: tela de seleção de categoria; falta fazer redirecionamento

--- frontend/src/Pages/Category.js
+++ frontend/src/Pages/Category.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import CleanOrderButton from '../components/CleanOrderButton/CleanOrderButton';
 import PayButton from '../components/PayButton/PayButton';
+import CategoryButton from '../components/CategoryButton/CategoryButton'
 
 export default function Category(props){
 
@@ -13,25 +14,53 @@ export default function Category(props){
         navigate('/review')
     }
 
+    
+
     return(
         <div className='d-flex w-100 h-100 row'>
             {/* Setor das categorias */}
-            <div className='d-fluid row w-100'
-                style={{height: '85vh'}}>
-                <div className='d-fluid col-2'/>
+            <div className='d-flex row w-100'
+                style={{height: '80vh', marginTop:'5vh'}}>
+                <div className='d-flex col-1'/>
                 {/* Coluna 1 de categorias */}
-                <div className='d-fluid col-4'>
-
+                <div className='d-flex col-5 justify-content-between align-items-center flex-column'>
+                    <div className='d-flex w-100 row'>
+                        <CategoryButton imageName='Vetor_Combo.png' 
+                                        category='Combos' 
+                                        onClick={() =>{
+                                            
+                                        }}/>
+                    </div>
+                    <div className='d-flex row w-100'>
+                        <CategoryButton imageName='Vetor_Lanche.png' 
+                                        category='Lanches' 
+                                        onClick={() =>{
+                                            
+                                        }}/>
+                    </div>
                 </div>
                 {/* Coluna 2 de categorias */}
-                <div className='d-fluid col-4'>
-
+                <div className='d-flex col-5 justify-content-between align-items-center flex-column'>
+                    <div className='d-flex w-100 row'>
+                        <CategoryButton imageName='Vetor_Bebida.png' 
+                                        category='Bebidas' 
+                                        onClick={() =>{
+                                            
+                                        }}/>
+                    </div>
+                    <div className='d-flex row w-100'>
+                        <CategoryButton imageName='Vetor_Sorvete.png' 
+                                        category='Sobremesas' 
+                                        onClick={() =>{
+                                            
+                                        }}/>
+                    </div>
                 </div>
-                <div className='d-fluid col-2'/>      
+                <div className='d-flex col-1'/>      
             </div>
             {/* Setor dos botões limpar Carrinho e Pagar */}
             <div className='d-flex row'
-                style={{height: '15vh'}}>
+                style={{height: '14vh', marginBottom:'1vh'}}>
                 <div className='d-flex justify-content-between align-items-center'>
                     <div className='d-flex col-1'/>
                     <CleanOrderButton emptyCart={props.emptyCart}/>
--- frontend/src/components/CategoryButton/CategoryButton.js
+++ frontend/src/components/CategoryButton/CategoryButton.js
@@ -16,16 +16,16 @@ export class CategoryButton extends Component {
     render() {
         return (
         <React.Fragment> 
-            <Button style={{height: '30vh', width: '30vh'}}
+            <Button style={{height: '35vh'}}
                 className='p-button-raised p-button-secondary d-flex justify-content-center align-items-center'
                 onClick={ () => {
                     this.props.onClick(this.props.category);
                     console.log(`TODO: Vai pra pagina de escolher ${this.props.category}`)}}>
                 <div className='d-flex justify-content-center align-items-center row w-100'
                     style={{height: '25vh'}}>
-                    <img className='d-flex justify-content-center align-items-center row w-100'
+                    <img className='d-flex justify-content-center align-items-center row'
                         src={require('../../assets/images/' + this.props.imageName)}
-                        style={{height: '20vh'}}></img>
+                        style={{height: '20vh', width: '20vh'}}></img>
                     <h2 className='d-flex justify-content-center align-items-center row w-100'
                         style={{height: '5vh'}}>{this.props.category}</h2>
                 </div>
