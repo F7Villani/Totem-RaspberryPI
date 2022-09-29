bba836ab70a8c2b478739a33da2e45d7a5564d64
Renan Reschke <renanreschke@hotmail.com>
2022-09-29 20:26:53 -0300

    feat: esqueleto tela de pagamento

--- frontend/src/Pages/Payment/Payment.js
+++ frontend/src/Pages/Payment/Payment.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './Payment.css'
 
 export default function Payment(props){
     return(
-        <div>
-            <p>Payment</p>
+        <div className='payment-background row'>
+            <h1 className='payment-header row'>Aponte o celular para o QR Code abaixo:</h1>
+            <img className='row' src={require('../../assets/images/qr_1.png')}
+                style={{height: '300px', width: '300px'}}/>
+            <p className='obs-text row'>Toque na tela quando o pagamento for concluído.</p>
         </div>
     )
 }
