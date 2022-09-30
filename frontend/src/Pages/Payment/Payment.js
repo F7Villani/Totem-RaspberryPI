8d15d926c9b40b31ceab1d2180bec8f45a6f3592
Renan Reschke <renanreschke@hotmail.com>
2022-09-29 21:09:33 -0300

    feat: add preço total na tela

--- frontend/src/Pages/Payment/Payment.js
+++ frontend/src/Pages/Payment/Payment.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './Payment.css'
 
 export default function Payment(props){
+
+    let [totalPrice, setTotalPrice] = useState()
+
+    useEffect(() => {
+        //run after component mount
+        let orderBody = getOrderBodyFromCart()
+        setTotalPrice(parseFloat(orderBody.totalPrice).toFixed(2).replace('.', ','))
+    },[])
 
     const navigate = useNavigate()
 
@@ -39,6 +47,7 @@ export default function Payment(props){
             <h1 className='payment-header row'>Aponte o celular para o QR Code abaixo:</h1>
             <img className='row' src={require('../../assets/images/qr_1.png')}
                 style={{height: '300px', width: '300px'}}/>
+            <h3 className='price-text row'>Total: R${totalPrice}</h3>
             <p className='obs-text row'>Toque na tela quando o pagamento for concluído.</p>
         </div>
     )
