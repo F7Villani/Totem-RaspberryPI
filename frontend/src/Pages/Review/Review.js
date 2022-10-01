3f6ec89526e7c71e7920d8963bdd289aadc513b5
Felipe Villani <felipevillani.2000@hotmail.com>
2022-09-30 21:50:26 -0300

    feat: começando a pagina review

--- frontend/src/Pages/Review/Review.js
+++ frontend/src/Pages/Review/Review.js
@@ -1,6 +1,8 @@
 import React, {useEffect} from 'react';
 import { useNavigate } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './Review.css';
 
 export default function Review(props){
 
@@ -13,8 +15,9 @@ export default function Review(props){
 
     return(
         <div>
-            <p>Review</p>
-            <br/>
+            <div className="top-bar top-bar-review d-flex row w-100 ">               
+                <h1 className='title-page'>Revisão do Pedido</h1>
+            </div>
             <p>
                 Carrinho: {props.cart}
             </p>
