bcc93bae3b08a7358052a10c169d22e46bf393d2
Felipe Villani <felipevillani.2000@hotmail.com>
2022-10-02 21:43:21 -0300

    feat: retirando mock do carrinho

--- frontend/src/Pages/Review/Review.js
+++ frontend/src/Pages/Review/Review.js
@@ -1,20 +1,12 @@
 import React, {useEffect} from 'react';
 import { useNavigate } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import CleanOrderButton from '../../components/CleanOrderButton/CleanOrderButton';
 import PayButton from '../../components/PayButton/PayButton';
 
 import './Review.css';
 import BackButton from '../../components/BackButton/BackButton';
 
 export default function Review(props){
-
-    useEffect(() => {
-        //run after component mount
-        //add automaticamente ao entrar na pagina 2 quantidades de um item pra teste do carrinho
-        //REMOVER
-        props.addItemToCart({'name': 'teste', 'id': '12jaflibu17b27b', 'price': 21.9, 'quantity': 2})
-    },[])
 
     const navigate = useNavigate();
 
