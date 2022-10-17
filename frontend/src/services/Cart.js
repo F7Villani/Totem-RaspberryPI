65cc1eae2f031433768f3c606d36b142fe933e57
Felipe Villani <felipevillani.2000@hotmail.com>
2022-10-16 21:25:33 -0300

    feat: migrando servico Cart

--- frontend/src/services/Cart.js
+++ frontend/src/services/Cart.js
@@ -1,5 +1,9 @@
 export class Cart{
 
+    Cart(){
+        localStorage.setItem('cart', '[]');
+    }
+
     getCart(){
         return localStorage.getItem("cart");
     }
@@ -8,20 +12,49 @@ export class Cart{
         localStorage.setItem('cart', '[]');
     }
 
-    addItem(item){ 
-
+    addItem(itemToAdd){ 
+        debugger
         let cart = this.getCart();
-        let {id} = item;
         
-        let existingItem = cart.find(cartItem => cartItem.id === id);
+        let existingItem = cart.find(cartItem => cartItem.id === itemToAdd.id);
         
         if (existingItem) {
-            cart.find((cartItem) => {
-                cartItem.quantity++;
+            cart.map((cartItem) => {
+                if(cartItem.id === itemToAdd.id)
+                {
+                    cartItem.quantity++;
+                }
+                return cartItem;
             })
-        } else {
-            item.quantity = 1;
-            cart.push(item);
+        } 
+        else {
+            itemToAdd.quantity = 1;
+            cart.push(itemToAdd);
+        }
+                
+        let stringCart = JSON.stringify(cart);
+        localStorage.setItem("cart", stringCart);
+    }
+
+    removeItem = (itemToRemove) => {
+        
+        let cart = this.getCart();
+        
+        let existingItem = cart.find(cartItem => cartItem.id === itemToRemove.id);
+        
+        if (existingItem && existingItem.quantity > 0) {
+            cart.map((cartItem) => {
+                if(cartItem.id === itemToRemove.id)
+                {
+                    cartItem.quantity--;
+                }
+                return cartItem;
+            })
+        } 
+        else {
+            cart.filter((item) => {
+                return item.id !== itemToRemove.id;
+            })
         }
                 
         let stringCart = JSON.stringify(cart);
